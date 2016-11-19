1aeb15a4011a50d7a729a2de18202b035c083893
sharonhe <mailshanshanhe@gmail.com>
2016-11-18 20:49:19 -0800

    adding script

--- Python_Scripts/check_dna.py
+++ Python_Scripts/check_dna.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python
 
+# check whether imput dna has undefined bases
+
+dna = raw_input('Enter DNA sequence:')
+if 'n' in dna or 'N' in dna:
+	nbases = dna.count('n') + dna.count('N')
+	print("dna sequence has %d undefined bases" % nbases)
+else:
+	print("dna sequence has no undefined bases")
