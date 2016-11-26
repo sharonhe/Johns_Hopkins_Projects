8a5e87ef1ce87a12f9cd8bccf813b80fc4a5eb5d
sharonhe <mailshanshanhe@gmail.com>
2016-11-26 14:54:18 -0800

    adding scripts

--- DNA_seq_Algorithm/Project4/shan4.py
+++ DNA_seq_Algorithm/Project4/shan4.py
@@ -3,3 +3,14 @@
 from genomeAssembly import deNovoAssambly
 import itertools
 
+# text = ["CCT", "CTT", "TGC", "TGG", "GAT", "ATT"]
+# assembly = deNovoAssambly (text = text)
+# print assembly.shortComSupstr()
+
+filename = "ads1_week4_reads.fq"
+assembly = deNovoAssambly (filename = filename)
+reads, _ = assembly.readFastq()
+sequence = assembly.greedyScs(reads, 8)
+counts = assembly.countNt(sequence)
+assembly.output(sequence)
+print counts
