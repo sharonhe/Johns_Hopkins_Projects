3bcfa08280c6acfd5e02e17962ccc7a4fb650ea8
sharonhe <mailshanshanhe@gmail.com>
2016-12-06 21:21:10 -0800

    merge fastq

--- Capstone_project/Download.sh
+++ Capstone_project/Download.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-fastq-dump -v --gzip SRR1554534
+fastq-dump -v --gzip --split-files SRR1554534
 
 /usr/bin/osascript -e 'tell application "Messages"
 send "SRR1554534 Downloaded yo" to buddy "sshe1986@gmail.com" of service "E:sshe1986@gmail.com"
 end tell'
 echo "Sent"
 
-fastq-dump -v --gzip SRR2071345
+fastq-dump -v --gzip --split-files SRR2071345
 
 /usr/bin/osascript -e 'tell application "Messages"
 send "SRR2071345 Downloaded yo" to buddy "sshe1986@gmail.com" of service "E:msshe1986@gmail.com"
 end tell'
 echo "Sent"
 
-fastq-dump -v --gzip SRR1554539
+fastq-dump -v --gzip --split-files SRR1554539
 
 /usr/bin/osascript -e 'tell application "Messages"
 send "SRR1554539 Downloaded yo" to buddy "sshe1986@gmail.com" of service "E:sshe1986@gmail.com"
 end tell'
 echo "Sent"
 
-fastq-dump -v --gzip SRR2071350
+fastq-dump -v --gzip --split-files SRR2071350
 
 /usr/bin/osascript -e 'tell application "Messages"
 send "SRR2071350 Downloaded yo" to buddy "sshe1986@gmail.com" of service "E:sshe1986@gmail.com"
