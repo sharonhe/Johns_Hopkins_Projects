446f0b3e309d24b51c344dbb2faa8b11f9d250a5
sharonhe <mailshanshanhe@gmail.com>
2016-12-12 22:06:32 -0800

    alignment

--- Capstone_project/Alignment/Alignment.sh
+++ Capstone_project/Alignment/Alignment.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
-# tophat2 -p 10 -o Ad_R2857_tophat -G /Volumes/ShanBook/Bioinformatics/Reference_genome/hg19_refFlat.gtf --transcriptome-index /Volumes/ShanBook/Bioinformatics/Reference_genome/hg19_refFlat/hg19_refFlat /Volumes/ShanBook/Bioinformatics/Reference_genome/hg19_bowtieindexed/hg19 Ad_R2857_1.fastq Ad_R2857_2.fastq
+# tophat2 -p 10 -o Ad_R2857_tophat -G /Volumes/EvaBook/Reference_Genome/hg19_refFlat.gtf --transcriptome-index /Volumes/EvaBook/Reference_Genome/hg19_refFlat/hg19_refFlat /Volumes/EvaBook/Reference_Genome/hg19_bowtieindexed/hg19 Ad_R2857_1.fastq Ad_R2857_2.fastq
 # echo "Ad_R2857 aligned"
 
-tophat2 -p 10 -o Ad_R3098_tophat --transcriptome-index /Volumes/ShanBook/Bioinformatics/Reference_genome/hg19_refFlat/hg19_refFlat /Volumes/ShanBook/Bioinformatics/Reference_genome/hg19_bowtieindexed/hg19 Ad_R3098_1.fastq Ad_R3098_2.fastq
-echo "Ad_R3098 aligned"
+# tophat2 -p 10 -o Ad_R3098_tophat -G /Volumes/EvaBook/Reference_Genome/hg19_refFlat.gtf --transcriptome-index /Volumes/EvaBook/Reference_Genome/hg19_refFlat/hg19_refFlat /Volumes/EvaBook/Reference_Genome/hg19_bowtieindexed/hg19 Ad_R3098_1.fastq Ad_R3098_2.fastq
+# echo "Ad_R3098 aligned"
 
-tophat2 -p 10 -o Ad_R3467_tophat --transcriptome-index /Volumes/ShanBook/Bioinformatics/Reference_genome/hg19_refFlat/hg19_refFlat /Volumes/ShanBook/Bioinformatics/Reference_genome/hg19_bowtieindexed/hg19 Ad_R3467_1.fastq Ad_R3467_2.fastq
+tophat2 -p 10 -o Ad_R3467_tophat -G /Volumes/EvaBook/Reference_Genome/hg19_refFlat.gtf --transcriptome-index /Volumes/EvaBook/Reference_Genome/hg19_refFlat/hg19_refFlat /Volumes/EvaBook/Reference_Genome/hg19_bowtieindexed/hg19 Ad_R3467_1.fastq Ad_R3467_2.fastq
 echo "Ad_R3467 aligned"
 
-tophat2 -p 10 -o Fe_R3452_tophat --transcriptome-index /Volumes/ShanBook/Bioinformatics/Reference_genome/hg19_refFlat/hg19_refFlat /Volumes/ShanBook/Bioinformatics/Reference_genome/hg19_bowtieindexed/hg19 Fe_R3452_1.fastq Fe_R3452_2.fastq
-echo "Fe_R3452 aligned"
+# tophat2 -p 10 -o Fe_R3452_tophat -G /Volumes/EvaBook/Reference_Genome/hg19_refFlat.gtf --transcriptome-index /Volumes/EvaBook/Reference_Genome/hg19_refFlat/hg19_refFlat /Volumes/EvaBook/Reference_Genome/hg19_bowtieindexed/hg19 Fe_R3452_1.fastq Fe_R3452_2.fastq
+# echo "Fe_R3452 aligned"
 
-tophat2 -p 10 -o Fe_R4706_tophat --transcriptome-index /Volumes/ShanBook/Bioinformatics/Reference_genome/hg19_refFlat/hg19_refFlat /Volumes/ShanBook/Bioinformatics/Reference_genome/hg19_bowtieindexed/hg19 Fe_R4706_1.fastq Fe_R4706_2.fastq
-echo "Fe_R4706 aligned"
+# tophat2 -p 10 -o Fe_R4706_tophat -G /Volumes/EvaBook/Reference_Genome/hg19_refFlat.gtf --transcriptome-index /Volumes/EvaBook/Reference_Genome/hg19_refFlat/hg19_refFlat /Volumes/EvaBook/Reference_Genome/hg19_bowtieindexed/hg19 Fe_R4706_1.fastq Fe_R4706_2.fastq
+# echo "Fe_R4706 aligned"
 
-tophat2 -p 10 -o Fe_R4707_tophat --transcriptome-index /Volumes/ShanBook/Bioinformatics/Reference_genome/hg19_refFlat/hg19_refFlat /Volumes/ShanBook/Bioinformatics/Reference_genome/hg19_bowtieindexed/hg19 Fe_R4707_1.fastq Fe_R4707_2.fastq
-echo "Fe_R4707 aligned"
-
+# tophat2 -p 10 -o Fe_R4707_tophat -G /Volumes/EvaBook/Reference_Genome/hg19_refFlat.gtf --transcriptome-index /Volumes/EvaBook/Reference_Genome/hg19_refFlat/hg19_refFlat /Volumes/EvaBook/Reference_Genome/hg19_bowtieindexed/hg19 Fe_R4707_1.fastq Fe_R4707_2.fastq
+# echo "Fe_R4707 aligned"
