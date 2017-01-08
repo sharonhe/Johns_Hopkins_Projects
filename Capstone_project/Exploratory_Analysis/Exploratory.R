cda49f4eae4bcae5efac4342402efdcca9698d1d
sharonhe <mailshanshanhe@gmail.com>
2017-01-07 22:13:13 -0800

    PCA

--- Capstone_project/Exploratory_Analysis/Exploratory.R
+++ Capstone_project/Exploratory_Analysis/Exploratory.R
@@ -14,9 +14,13 @@ boxplot(exp_data[ ,2:7],col=2,range=0)
 boxplot(log2(exp_data[ ,2:7]+1),col=2,range=0)
 
 # Make a histogram for each sample to see whether data need to be transformed
-par(mfrow=c(1,2))
-hist(log2(exp_data[ ,2]+1),col=2)
-hist(log2(exp_data[ ,3]+1),col=2)
+par(mfrow=c(2,3))
+hist(log2(exp_data[ ,1]+1),col=2, main = "R3098")
+hist(log2(exp_data[ ,2]+1),col=2, main = "R2857")
+hist(log2(exp_data[ ,3]+1),col=2, main = "R3467")
+hist(log2(exp_data[ ,4]+1),col=2, main = "R3452")
+hist(log2(exp_data[ ,5]+1),col=2, main = "R4706")
+hist(log2(exp_data[ ,6]+1),col=2, main = "R4707")
 
 # assign row names
 row.names(exp_data) <- make.names(exp_data[,1], unique = TRUE)
@@ -26,42 +30,55 @@ dim(exp_data)
 # Transform the data
 edata <- log2(exp_data + 1)
 
-# transpose and center the data
-edata_t <- t(edata)
-edata_t_centered <- edata_t - rowMeans(edata_t)
+# Center the data
+edata_centered <- edata - rowMeans(edata)
 
 # calculate the singular vectors
-svd1 = svd(edata_t_centered)
+svd2 = svd(edata)
 
 # Look at the percent variance explained
-plot(svd1$d,ylab="Singular value",col=2)
+par(mfrow=c(1,1))
+plot(svd2$d^2/sum(svd2$d^2),ylab="Percent Variance Explained",col=2)
 
 # Plot top two principal components
 par(mfrow=c(1,2))
-plot(svd1$v[,1],col=2,ylab="1st PC")
-plot(svd1$v[,2],col=2,ylab="2nd PC")
+plot(svd2$v[1,],col=2,ylab="1st PC")
+plot(svd2$v[2,],col=2,ylab="2nd PC")
 
 # color by different variables to see if clusters stand out
 palette()  # the colours and their order
 
 # by group
-par(mfrow=1)
-plot(svd1$v[,1],svd1$v[,2],ylab="2nd PC",
+par(mfrow=c(1,2))
+plot(svd2$v[,1],svd2$v[,2],ylab="2nd PC",
      xlab="1st PC",col=as.numeric(pheno_data$group))
-legend("topleft", c("adult", "fetus"), pch = c(1,1), col = c("black", "red"))
+legend("bottomright", c("adult", "fetus"), pch = c(1,1), col = c("black", "red"))
 
 # by sex
-plot(svd1$v[,1],svd1$v[,2],ylab="2nd PC",
+plot(svd2$v[,1],svd2$v[,2],ylab="2nd PC",
      xlab="1st PC",col=as.numeric(pheno_data$sex))
-legend("topleft", c("female", "male"), pch = c(1,1), col = c("black", "red"))
+legend("bottomright", c("female", "male"), pch = c(1,1), col = c("black", "red"))
 
 # by race
-plot(svd1$v[,1],svd1$v[,2],ylab="2nd PC",
+par(mfrow=c(1,2))
+plot(svd2$v[,1],svd2$v[,2],ylab="2nd PC",
      xlab="1st PC",col=as.numeric(pheno_data$race))
-legend("topleft", c("AA", "HISP"), pch = c(1,1), col = c("black", "red"))
+legend("bottomright", c("AA", "HISP"), pch = c(1,1), col = c("black", "red"))
 
 # by RIN
-plot(svd1$v[,1],svd1$v[,2],ylab="2nd PC",
+plot(svd2$v[,1],svd2$v[,2],ylab="2nd PC",
      xlab="1st PC",col=pheno_data$RIN)
-legend("topleft", c("9.6", "8.3", "8.6", "8.4", "5.3", "9.0" ), pch = c(1,1), col = c(9.6, 8.3, 8.6, 8.4, 5.3, 9.0))
+legend("bottomright", c("9.6", "8.3", "8.6", "8.4", "5.3", "9.0" ), pch = c(1,1), col = c(9.6, 8.3, 8.6, 8.4, 5.3, 9.0))
+
+# transpose and center the data
+# edata_t <- t(edata)
+# edata_t_centered <- edata_t - rowMeans(edata_t)
+
+# calculate the singular vectors
+# svd1 = svd(edata_t_centered)
+
+
+
+
+
 
